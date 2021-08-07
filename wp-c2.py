689e0826917c174a4e4d293420559ac3d6796f12
accidentalrebel <accidentalrebel@gmail.com>
2021-08-07 17:02:51 +0800

    Initial curl command tests.

---
+++ wp-c2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+curl_command = """
+curl 'http://127.0.0.3/wp-comments-post.php' \
+  -H 'Connection: keep-alive' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'sec-ch-ua: "Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"' \
+  -H 'sec-ch-ua-mobile: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'Origin: http://127.0.0.3' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Referer: http://127.0.0.3/2021/08/06/hello-world/' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  --data-raw 'author=TestName&email=test%40email.com&url=&submit=Post+Comment&comment_post_ID=1&comment_parent=0&comment=TestComment12' \
+  --compressed -Ls -o /dev/null -w %{url_effective}
+"""
+curl_output = subprocess.check_output(curl_command, shell=True)
+print(curl_output)
+
+fetch_comments_command = """
+curl '""" \
++ curl_output.decode() + \
+"""' \
+  -H 'Connection: keep-alive' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'sec-ch-ua: "Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"' \
+  -H 'sec-ch-ua-mobile: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'Sec-Fetch-Site: none' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  --compressed
+"""
+curl_output = subprocess.check_output(fetch_comments_command, shell=True)
+print("\n###\n\n")
+print(curl_output)
