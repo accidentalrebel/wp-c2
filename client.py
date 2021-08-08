b2b099309df512bf07af5729caf0168ebd50650f
accidentalrebel <accidentalrebel@gmail.com>
2021-08-08 22:43:09 +0800

    Can now delay according to time_diff.

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from wpc2 import submit_comment
+import datetime
+import time
 
 seed = "abcdef"
 commands = [ "info", "exfil", "delete" ]
@@ -8,5 +10,22 @@ commands = [ "info", "exfil", "delete" ]
 target_blog = "http://127.0.0.3/"
 sync_channel = "2021/08/06/hello-world/"
 
-response = submit_comment("TestComment1")
-print(response)
+time_slot = "1:11"
+time_slot_seconds = int(time_slot.split(":")[1])
+current_time = datetime.datetime.now().time()
+
+time_diff = 0
+if current_time.second < time_slot_seconds:
+    time_diff = time_slot_seconds - current_time.second
+else:
+    time_diff = 60 - current_time.second + time_slot_seconds
+    
+print(str(current_time.second) + " ?- " + str(time_slot_seconds))
+print(str(time_diff))
+
+time.sleep(time_diff)
+
+print("Triggering at: " + str(datetime.datetime.now().time()))
+
+# response = submit_comment("TestComment1")
+# print(response)
